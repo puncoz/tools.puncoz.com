7603f05a1de724172ea21bf47f00468a044dee68
Puncoz Nepal <puncoz138@gmail.com>
2025-02-09 00:25:10 +0900

    tldraw configure

---
+++ src/app/draw/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { FunctionComponent } from "react"
+import { Tldraw } from "tldraw"
+import "tldraw/tldraw.css"
+
+const DrawPage: FunctionComponent = () => {
+  return (
+    <div className="fixed inset-0">
+      <Tldraw/>
+    </div>
+  )
+}
+
+export default DrawPage
